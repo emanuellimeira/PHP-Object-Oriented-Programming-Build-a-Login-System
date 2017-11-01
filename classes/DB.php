d89b5faacaf61383f8c7d2e7384b531a65b84580
Thymopat <Thymopat@users.noreply.github.com>
2017-11-01 16:08:41 +0100

    some brackets issues

--- classes/DB.php
+++ classes/DB.php
@@ -1,171 +1,156 @@
 <?php
-
-/**
- * Database Class
- */
-class DB {
-
-  private static $_instance = null;
-
-  private $_pdo,
-          $_query,
-          $_error = false,
-          $_results,
-          $_count = 0;
-
-
-  private function __construct()
+  class db
   {
-    try {
-      $this->_pdo = new PDO('mysql:host='. Config::get('mysql/host') .';dbname='. Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
-      //echo "conectado!";
-    } catch (PDOException $e) {
-      die($e->getMessage());
+    private static  $_instance  = null;
+    private         $_pdo,
+                    $_query,
+                    $_error     = false,
+                    $_results,
+                    $_count     = 0;
+
+    private function __construct()
+    {
+      try
+      {
+        $this->_pdo = new PDO('mysql:host='. Config::get('mysql/host') .';dbname='. Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
+      }
+      catch (PDOException $e)
+      {
+        die($e->getMessage());
+      }
     }
 
-
-  }
-
-  public static function getInstance()
-  {
-
-    if(!isset(self::$_instance)) {
-      self::$_instance = new DB();
+    public static function getInstance()
+    {
+      if(!isset(self::$_instance))
+      {
+        self::$_instance = new DB();
+      }
+      return self::$_instance;
     }
 
-    return self::$_instance;
-  }
-
-  public function query($sql, $params = array())
-  {
-    $this->_error = false;
-
-    if($this->_query = $this->_pdo->prepare($sql)) {
-      //echo "Sucesso!";
-      $x = 1;
-      if(count($params)) {
-        foreach($params as $param) {
-          $this->_query->bindValue($x, $param);
-          $x++;
+    public function query($sql, $params = array())
+    {
+      $this->_error = false;
+      if($this->_query = $this->_pdo->prepare($sql))
+      {
+        $x = 1;
+        if(count($params))
+        {
+          foreach($params as $param)
+          {
+            $this->_query->bindValue($x, $param);
+            $x++;
+          }
+        }
+        if($this->_query->execute())
+        {
+          $this->_results   = $this->_query->fetchAll(PDO::FETCH_OBJ);
+          $this->_count     = $this->_query->rowCount();
+        }
+        else
+        {
+          $this->_error = true;
         }
       }
-      if($this->_query->execute()) {
-        //echo "Sucesso execute()!";
-        $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
-        $this->_count = $this->_query->rowCount();
-      } else {
-        $this->_error = true;
-      }
+      return $this;
     }
-    return $this;
-  }
 
-  public function action($action, $table, $where = array())
-  {
-    if (count($where) === 3) {
-      $operators = array('=','>','<','>=', '<=');
-
-      $field    = $where[0];
-      $operator = $where[1];
-      $value    = $where[2];
-
-
-    if (in_array($operator, $operators)) {
-      $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
-
-      if (!$this->query($sql, array($value))->error()) {
-        return $this;
+    public function action($action, $table, $where = array())
+    {
+      if(count($where) === 3) {
+        $operators = array('=','>','<','>=', '<=');
+        $field    = $where[0];
+        $operator = $where[1];
+        $value    = $where[2];
+        if(in_array($operator, $operators))
+        {
+          $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+          if(!$this->query($sql, array($value))->error())
+          {
+            return $this;
+          }
+        }
       }
+      return false;
     }
-  }
-    return false;
-  }
 
-  public function get($table, $where)
-  {
-    return $this->action('SELECT *', $table, $where);
-  }
+    public function get($table, $where)
+    {
+      return $this->action('SELECT *', $table, $where);
+    }
 
-  public function delete($table, $where)
-  {
-    return $this->action('DELETE', $table, $where);
-  }
+    public function delete($table, $where)
+    {
+      return $this->action('DELETE', $table, $where);
+    }
 
-  public function insert($table, $fields = array())
-  {
-    //if(count($fields)) {
-      $keys = array_keys($fields);
+    public function insert($table, $fields = array())
+    {
+      $keys   = array_keys($fields);
       $values = null;
-      $x = 1;
+      $x      = 1;
 
-      foreach($fields as $field) {
+      foreach($fields as $field)
+      {
         $values .= '?';
-        if($x < count($fields)) {
+        if($x < count($fields))
+        {
           $values .= ', ';
         }
         $x++;
       }
 
-      //die($values);
-
       $sql = "INSERT INTO {$table} (`" . implode('`,`' , $keys) . "`) VALUES ({$values})";
-      //echo $sql;
 
-      if(!$this->query($sql, $fields)->error()) {
+      if(!$this->query($sql, $fields)->error())
+      {
         return true;
       }
-    //}
-    return false;
-  }
+      return false;
+    }
 
-  public function update($table, $id, $fields)
-  {
-    $set = '';
-    $x = 1;
-
-    foreach($fields as $name => $value) {
-      $set .= "{$name} = ?";
-      if($x < count($fields)) {
-        $set .= ', ';
+    public function update($table, $id, $fields)
+    {
+      $set  = '';
+      $x    = 1;
+      foreach($fields as $name => $value)
+      {
+        $set .= "{$name} = ?";
+        if($x < count($fields))
+        {
+          $set .= ', ';
+        }
+        $x++;
       }
-      $x++;
+
+      $sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
+
+      if(!$this->query($sql, $fields)->error())
+      {
+        return true;
+      }
+      return false;
     }
-    //die($set);
 
-    $sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
-
-    //echo $sql;
-
-    if (!$this->query($sql, $fields)->error()) {
-      return true;
+    public function results()
+    {
+      return $this->_results;
     }
-    return false;
 
+    public function first()
+    {
+      return $this->results()[0];
+    }
+
+    public function error()
+    {
+      return $this->_error;
+    }
+
+    public function count()
+    {
+      return $this->_count;
+    }
   }
-
-  public function results()
-  {
-    return $this->_results;
-  }
-
-  public function first()
-  {
-    // return $this->_results[0];
-    return $this->results()[0];
-  }
-
-  public function error()
-  {
-    return $this->_error;
-  }
-
-  public function count()
-  {
-    return $this->_count;
-  }
-
-
-
-
-
-} // fim class
+?>
